50f8f66554b6290dd0dd6aa20b58bcc026d932e5
isyageek <v.ferrava@usi.ch>
2019-09-15 09:38:28 +0200

    Update prelive

--- strstack.c
+++ strstack.c
@@ -1,9 +1,21 @@
 #include"strstack.h"
 #include <stdio.h>
-#ihnclude <stdlib.h> 
+#include <stdlib.h> 
 
 
-void strstack_use_buffer(char * mem, size_t mem_size);
-void strstack_clear();
-int strstack_push(const char * s);
-const char * strstack_pop();
+void strstack_use_buffer(char * mem, size_t mem_size){
+
+}
+
+void strstack_clear(){
+
+}
+
+
+int strstack_push(const char * s){
+    return 0;
+}
+
+const char * strstack_pop(){
+    return NULL;
+}
